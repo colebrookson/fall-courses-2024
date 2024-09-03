55ced59dbc69696b3208f2f76b76bbf73910a9f2
Cole Brookson <cole.brookson@gmail.com>
2024-09-03 14:34:57 -0400

    some of the power function

--- s&ds663/homework/code/funs.c
+++ s&ds663/homework/code/funs.c
@@ -1,3 +1,29 @@
 #include "sds663print.h" // We'll use sds663print for printing
 #include <stdio.h>
 #include <math.h>
+
+//
+// mypow
+// returns x^n
+//
+// Input:
+// * x  : real number of double precision length 1
+// * n  : integer
+//
+double mypow(double x, int n)
+{
+    int i;
+    double res;
+    /* corner case of exponent 0 */
+    if (n == 0)
+    {
+        res = 1;
+    }
+    /* the loop goes through all n*/
+    for (i = 1; i <= n; i++)
+    {
+        /* multiply x by itself*/
+        res = x * x;
+    }
+    return res;
+}
